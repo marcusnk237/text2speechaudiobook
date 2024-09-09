f94e4855fac8b2d46cdc2e3ea8ca712f8c5d8159
marcusnk237 <74594432+marcusnk237@users.noreply.github.com>
2024-09-09 12:43:10 +0200

    Add files via upload

---
+++ pdf2audio.py
@@ -0,0 +1,17 @@
+import pdfplumber as pp
+from gtts import gTTS
+
+def extractText(file_path):
+    pdf_text = ''
+    with pp.open(file_path) as pdf:
+        for page in pdf.pages:
+            pdf_text += page.extract_text()
+    return pdf_text
+
+
+def text2mp3 (file_path , save_path,  lang ='fr'):
+
+    pdf2text = extractText(file_path)
+    tts = gTTS(text=pdf2text, lang=lang)
+    tts.save(save_path)
+    
